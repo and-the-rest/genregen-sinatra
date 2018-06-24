e94289f0fa7378f97cf527f1e4c38089c410cc42
William Woodruff <william@yossarian.net>
2018-06-24 15:30:58 -0400

    app: RuboCop fixes

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require 'sinatra'
-require 'haml'
-require 'genregen'
-require 'json'
+require "sinatra"
+require "haml"
+require "genregen"
+require "json"
 
-get '/' do
-	haml :index, :locals => { :genre => GenreGen.generate }
+get "/" do
+  haml :index, locals: { genre: GenreGen.generate }
 end
 
-get '/json' do
-	content_type :json
-	{ :genre => GenreGen.generate }.to_json
+get "/json" do
+  content_type :json
+  { genre: GenreGen.generate }.to_json
 end
 
 not_found do
-	haml :error, :locals => { :error => "404" }
+  haml :error, locals: { error: "404" }
 end
